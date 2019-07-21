b1da00badcda682425c5e632d60a226582d12095
thewebsitelevel <43106203+thewebsitelevel@users.noreply.github.com>
2019-07-22 01:47:58 +0530

    CSS

--- app.js
+++ app.js
@@ -136,21 +136,21 @@ app.get('/fetchgallery', (req,res,next) =>{
 
 
 
-// app.post('/calculate', (req,res, next) =>{
-//     console.log(req.body);
-//     let length = Number(req.body.length);
-//     let height = Number(req.body.height);
-//     let lengthUnit = Number(req.body.lu);
-//     let heightUnit = Number(req.body.hu);
-//     let thickness = Number(req.body.tu);
-//     let country = Number(req.body.country);
+app.post('/calculate', (req,res, next) =>{
+    console.log(req.body);
+    let length = Number(req.body.length);
+    let height = Number(req.body.height);
+    let lengthUnit = Number(req.body.lu);
+    let heightUnit = Number(req.body.hu);
+    let thickness = Number(req.body.tu);
+    let country = Number(req.body.country);
     
-//     console.log(length*height*thickness/(lengthUnit * heightUnit* country));
-//     let number =length*height*thickness/(lengthUnit * heightUnit* country)
-//     console.log(number)
-//     res.send(number)
-//     //  console.log(Number(req.body.length)*Number(req.body.lengthunit)*Number(req.body.height)*Number(req.body.heightunit)*Number(req.body.thicknessunit))
-// })
+    console.log(length*height*thickness/(lengthUnit * heightUnit* country));
+    let number =length*height*thickness/(lengthUnit * heightUnit* country)
+    console.log(number)
+    res.send(number)
+    //  console.log(Number(req.body.length)*Number(req.body.lengthunit)*Number(req.body.height)*Number(req.body.heightunit)*Number(req.body.thicknessunit))
+})
 
 
 
